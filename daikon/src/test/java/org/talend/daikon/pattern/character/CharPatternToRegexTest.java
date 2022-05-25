d1645a674c124303e95ec13adaab57846c8505b2
fabballe <fabballe@talend.com>
2022-05-25 11:19:50 +0200

    fix(): switch ScriptEngine to Graal VM JS engine

--- daikon/src/test/java/org/talend/daikon/pattern/character/CharPatternToRegexTest.java
+++ daikon/src/test/java/org/talend/daikon/pattern/character/CharPatternToRegexTest.java
@@ -1,5 +1,9 @@
 package org.talend.daikon.pattern.character;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import javax.script.ScriptEngine;
@@ -7,10 +11,6 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CharPatternToRegexTest {
 
